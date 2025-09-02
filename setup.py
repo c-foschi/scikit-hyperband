08551593b40f24850bc01c2c8cd55ab85bb175f4
carlo <39349171+c-foschi@users.noreply.github.com>
2025-09-02 12:35:36 +0200

    Update setup.py
    delete requirements for setup

--- setup.py
+++ setup.py
@@ -5,19 +5,6 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
-
-try:
-    import numpy
-except ImportError:
-    print('numpy is required during installation')
-    sys.exit(1)
-
-try:
-    import scipy
-except ImportError:
-    print('scipy is required during installation')
-    sys.exit(1)
-
 setup(name='scikit-hyperband',
       version='0.0.1',
       description='A scikit-learn compatible implementation of hyperband for model selection',
